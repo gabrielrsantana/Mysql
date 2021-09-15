413097fe9eea52456141abae1d400b39d39d2d87
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2021-09-15 18:05:41 -0300

    Add files via upload

---
+++ padaria_script.sql
@@ -0,0 +1,25 @@
+-- create database padaria_manoel;
+-- use padaria_manoel;
+-- drop table produtos;
+-- create table  if not exists produtos(
+--     id int not null primary key auto_increment,
+--     nome varchar(20),
+--     valor float,
+--     estoque int
+-- );
+
+-- drop table funcionarios;
+-- create table if not exists funcionarios(
+-- id int not null primary key auto_increment,
+-- cpf varchar(11),
+-- nome varchar(20),
+-- setor varchar(20)
+-- );
+use padaria_manoel;
+insert into funcionarios(cpf,nome,setor) values('123456789','manuel','recepcao');
+insert into funcionarios(cpf,nome,setor) values('5rt456789','carol','atendimento');
+insert into produtos(nome,valor,estoque) values('bolo',35.20,20);
+insert into produtos(nome,valor,estoque) values('cheeseburge',15.20,10);
+insert into clientes(nome,cpf) values('JoaoCliente','86446789');
+insert into clientes(nome,cpf) values('MariaCliente','67456745');
+
